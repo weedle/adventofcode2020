3af8e4400aa0fba7004e25edb31dfba3723f6ed5
Kevin <mercury0110111@gmail.com>
2020-12-06 06:41:54 +0000

    Cleaned up problem 2

--- p2/sol.py
+++ p2/sol.py
@@ -1,23 +1,31 @@
-file1 = open('data.txt', 'r') 
-lines = file1.readlines() 
-good = 0
+#!/usr/bin/python3
+# Load file
+datafile = open('data.txt', 'r')
+lines = datafile.readlines()
 
-def do_line(line):
-    global good
+def parseLine(line):
     data = line.split(' ')
-    
     minVal, maxVal = data[0].split('-')
     charReq = data[1][0]
     password = data[2]
+    return (int(minVal), int(maxVal), charReq, password)
+
+def checkLine(line):
+    good = 0
+    minVal, maxVal, charReq, password = parseLine(line)
     count = password.count(charReq)
-    
-    if(count >= int(minVal) and count <= int(maxVal)):
-        good += 1
+
+    if count >= minVal and count <= maxVal:
         print(' passed: {}, has {} {}\'s and needs {}-{}'.format(password, count, charReq, minVal, maxVal))
+        return 1
     else:
-        print('    failed: {}, has {} {}\'s but needs {}-{}'.format(password, count, charReq, minVal, maxVal))
+        print('     failed: {}, has {} {}\'s but needs {}-{}'.format(password, count, charReq, minVal, maxVal))
+        return 0
 
-for line in lines:
-    do_line(line)
+def countGoodPasswords(lines):
+    good = 0
+    for line in lines:
+        good += checkLine(line.strip())
+    return good
 
-print(str(good) + ' passwords')
+print(str(countGoodPasswords(lines)) + ' passwords')
